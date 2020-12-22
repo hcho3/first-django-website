01b46fa067420d027778173a0202719ca04799b8
Hyunsu Cho <chohyu01@cs.washington.edu>
2020-12-22 01:07:12 -0800

    Add the front page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('polls/', include('polls.urls'), name='polls'),
+    path('admin/', admin.site.urls, name='admin'),
 ]
